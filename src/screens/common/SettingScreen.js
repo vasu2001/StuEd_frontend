b1fce9e3d1923bfaa6a3ba0b4bbe40f988aeb691
Sambbhavjain <58338193+Sambbhavjain@users.noreply.github.com>
2020-04-18 00:48:28 +0530

    minor

--- src/screens/common/SettingScreen.js
+++ src/screens/common/SettingScreen.js
@@ -31,102 +31,116 @@ export default class SettingScreen extends React.Component {
           imageStyle={{resizeMode: 'stretch'}}
           source={require('../../assets/bg2.png')}
           style={styles.image}>
-          <Text style={styles.text}>Settings</Text>
-          <View style={{flex: 2}}>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => {
-                this.setState({show: true});
-              }}>
-              <Image
-                style={styles.userimage}
-                source={require('../../assets/latestusername.png')}
-              />
-            </TouchableOpacity>
+          <View
+            style={{
+              flex: 0.7,
+              justifyContent: 'flex-end',
+              paddingLeft: 30,
+              paddingBottom: 3,
+            }}>
+            <Text style={styles.text}>Settings</Text>
           </View>
-          <Modal transparent={true} visible={this.state.show}>
-            <View style={styles.modaldesign}>
-              <TextInput
-                placeholder="Enter new Username"
-                style={styles.modalfield}
-                placeholderTextColor="#7A898B"
-                keyboardType={'default'}
-                underlineColorAndroid={'black'}
-              />
-              <View
-                style={{
-                  flexDirection: 'row',
-                  marginVertical: -10,
+          <View
+            style={{
+              flex: 2,
+              paddingTop: 20,
+            }}>
+            <View style={{flex: 1}}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => {
+                  this.setState({show: true});
                 }}>
-                <View style={{marginLeft: 70}}>
-                  <Button
-                    title="Close"
-                    onPress={() => {
-                      this.setState({
-                        show: false,
-                      });
-                    }}
-                  />
-                </View>
-                <View style={{marginLeft: 90}}>
-                  <Button title="Save" />
+                <Image
+                  style={styles.userimage}
+                  source={require('../../assets/latestusername.png')}
+                />
+              </TouchableOpacity>
+            </View>
+            <Modal transparent={true} visible={this.state.show}>
+              <View style={styles.modaldesign}>
+                <TextInput
+                  placeholder="Enter new Username"
+                  style={styles.modalfield}
+                  placeholderTextColor="#7A898B"
+                  keyboardType={'default'}
+                  underlineColorAndroid={'black'}
+                />
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    marginVertical: -10,
+                  }}>
+                  <View style={{marginLeft: 70}}>
+                    <Button
+                      title="Close"
+                      onPress={() => {
+                        this.setState({
+                          show: false,
+                        });
+                      }}
+                    />
+                  </View>
+                  <View style={{marginLeft: 90}}>
+                    <Button title="Save" />
+                  </View>
                 </View>
               </View>
+            </Modal>
+            <View style={{flex: 1}}>
+              <TouchableOpacity style={styles.button}>
+                <Image
+                  style={styles.phonenum}
+                  source={require('../../assets/latestphone.png')}
+                />
+              </TouchableOpacity>
             </View>
-          </Modal>
-          <View style={{flex: 1}}>
-            <TouchableOpacity style={styles.button}>
-              <Image
-                style={styles.phonenum}
-                source={require('../../assets/latestphone.png')}
-              />
-            </TouchableOpacity>
-          </View>
-          <View style={{flex: 1}}>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => {
-                Linking.openURL('https://stuedstartup.wordpress.com/faqs/');
-              }}>
-              <Image
-                style={styles.help}
-                source={require('../../assets/help.png')}
-              />
-            </TouchableOpacity>
-          </View>
-          <View style={{flex: 1}}>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => {
-                Linking.openURL(
-                  'https://stuedstartup.wordpress.com/terms-and-conditons/',
-                );
-              }}>
-              <Image
-                style={styles.terms}
-                source={require('../../assets/latestterms.png')}
-              />
-            </TouchableOpacity>
-          </View>
-          <View style={{flex: 1}}>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => {
-                Linking.openURL('https://stuedstartup.wordpress.com/about/');
-              }}>
-              <Image
-                style={styles.contact}
-                source={require('../../assets/latestcontact.png')}
-              />
-            </TouchableOpacity>
-          </View>
-          <View style={{flex: 1}}>
-            <TouchableOpacity style={styles.button}>
-              <Image
-                style={styles.logout}
-                source={require('../../assets/logout.png')}
-              />
-            </TouchableOpacity>
+            <View style={{flex: 1}}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => {
+                  Linking.openURL('https://stuedstartup.wordpress.com/faqs/');
+                }}>
+                <Image
+                  style={styles.help}
+                  source={require('../../assets/help.png')}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={{flex: 1}}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => {
+                  Linking.openURL(
+                    'https://stuedstartup.wordpress.com/terms-and-conditons/',
+                  );
+                }}>
+                <Image
+                  style={styles.terms}
+                  source={require('../../assets/latestterms.png')}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={{flex: 1}}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => {
+                  Linking.openURL('https://stuedstartup.wordpress.com/about/');
+                }}>
+                <Image
+                  style={styles.contact}
+                  source={require('../../assets/latestcontact.png')}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={{flex: 1}}>
+              <TouchableOpacity style={styles.button}>
+                <Image
+                  style={styles.logout}
+                  source={require('../../assets/logout.png')}
+                />
+              </TouchableOpacity>
+            </View>
           </View>
         </ImageBackground>
       </View>
@@ -149,49 +163,33 @@ export default class SettingScreen extends React.Component {
     text: {
       color: 'white',
       fontSize: 33,
-      marginLeft: 31,
-      marginTop: 160,
       textShadowColor: 'white',
       letterSpacing: 1,
     },
-    button: {
-      margin: 40,
-    },
+    button: {},
     userimage: {
       height: 55,
       width: 500,
-      marginTop: 20,
-      marginHorizontal: -55,
     },
     phonenum: {
       height: 55,
       width: 500,
-      marginHorizontal: -55,
-      marginVertical: -55,
     },
     help: {
       height: 55,
       width: 500,
-      marginHorizontal: -55,
-      marginVertical: -45,
     },
     terms: {
       height: 55,
       width: 500,
-      marginHorizontal: -55,
-      marginVertical: -35,
     },
     contact: {
       height: 55,
       width: 500,
-      marginHorizontal: -55,
-      marginVertical: -30,
     },
     logout: {
       height: 55,
       width: 500,
-      marginHorizontal: -55,
-      marginVertical: -30,
     },
     modaldesign: {
       backgroundColor: '#ffffff',
